9067876232ab4112006b2cfb046ae5d8e07ec041
Dobrynia100 <dobrynya2001@gmail.com>
2022-11-07 16:26:23 +0300

    Отмена некоторых изменений

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import requests as req
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 import string
-import array
 
 token = config.settings['TOKEN']    # присваиваем переменной значение токена из файла конфига
 group_id=config.settings['group_id']             # id выбранной для работы бота группы
@@ -37,18 +36,23 @@ def toFixed(numObj, digits=0):
 def print_weather(period, i):  # функция получения текущего города
     # print(data)
     data = get_weather(period)
-    if period == 1 or 7:
-        current_weather = data['data'][i]  # выбираем нужную нам часть с данными
-        date = current_weather['datetime']
-        desc = current_weather['weather']['description']
-        wind = current_weather['wind_cdir_full']
-        wind_spd = current_weather['wind_spd']
-        wind_spd = toFixed(wind_spd, 2)
-        if period == 1:
+    if period == 1:
+            current_weather = data['data'][i]  # выбираем нужную нам часть с данными
+            date = current_weather['datetime']
+            desc = current_weather['weather']['description']
+            wind = current_weather['wind_cdir_full']
+            wind_spd = current_weather['wind_spd']
+            wind_spd = toFixed(wind_spd, 2)
             city = current_weather['city_name']
             temp = current_weather['app_temp']
             weather = date + '\n' + desc + ' - ' + str(temp) + 'C \n' + "Ветер - " + wind + '\nСкорость ветра - ' + str(wind_spd) + ' м/с'
-        elif period == 7:
+    elif period == 7:
+            current_weather = data['data'][i]  # выбираем нужную нам часть с данными
+            date = current_weather['datetime']
+            desc = current_weather['weather']['description']
+            wind = current_weather['wind_cdir_full']
+            wind_spd = current_weather['wind_spd']
+            wind_spd = toFixed(wind_spd, 2)
             temp = current_weather['app_max_temp']
             weather = date + '\n' + desc + ' - ' + 'макс. температура - ' + str(temp) + 'C \n' + "Ветер - " + wind + '\nСкорость ветра - ' + str(wind_spd) + ' м/с'
     elif period == 6 or 3:
@@ -99,8 +103,7 @@ def menu(reseived_message):
 
     elif reseived_message == "текущая":
         print("Текущая погода отправлена в ", chat)
-        weather = get_apis()
-        write_message(chat, get_numbers(weather))
+        write_message(chat,  print_weather(1,0))
 
 
 for event in longpoll.listen():                               # ждем от сервера ответа о произошедшем событии
